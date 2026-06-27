1bc4db4449cd972d481a18a6b0b2bae0a427f90c
sabrinaskaa <askach44@gmail.com>
2026-06-28 05:35:57 +0700

    fixing for deploy

--- src/Utils/AxiosInstance.jsx
+++ src/Utils/AxiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3001",
   headers: {
     "Content-Type": "application/json",
   },
